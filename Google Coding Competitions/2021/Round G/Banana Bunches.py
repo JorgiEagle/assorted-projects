764ff85b88176e4741f0af63547cd827102ce01a
Jack Jorgensen <jack.jorgensen@student.manchester.ac.uk>
2021-11-16 19:35:51 +0000

    mousey commit

--- Google Coding Competitions/2021/Round G/Banana Bunches.py
+++ Google Coding Competitions/2021/Round G/Banana Bunches.py
@@ -2,24 +2,10 @@ for test in range(int(input())):
     case = 'Case #' + str(test+1) + ':'
     number_of_trees, capacity_K = map(int, input().split())
     bananas_on_trees = list(map(int, input().split()))
-    max_bananas = max(bananas_on_trees)
-    total_bananas = sum(bananas_on_trees)
-    if max_bananas == capacity_K:
-        print(case, '1')
-        continue
-    if total_bananas < capacity_K:
-        print(case, '-1')
-        continue
 
-    current_best = float('inf')
-
-    for x in range(number_of_trees):
-        for y in range(x, number_of_trees):
-            if sum(bananas_on_trees[:x]) + sum((bananas_on_trees)[y:]) == capacity_K:
-                print(case, x+(number_of_trees-y)-1)
-                break
-        else:
-            continue
-        break
-    else:
-        print(case, '-1')
+    # a list storing the shortest array length that has the sum equal to the index which it is at,
+    # Could be presented in dictionary format where the key is the sum ,and the value is the shortest array
+    # whose sum is equal to the key
+    Best_min_Sum = {}
+    for i in range(capacity_K):
+        Best_min_Sum[i] = float('inf')

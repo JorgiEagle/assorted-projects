26fcf54a5f1812449b2f25e9a3ee32b4f3e59329
Jack Jorgensen <jack.jorgensen@student.manchester.ac.uk>
2023-04-08 14:18:12 +0100

    Added some stuff

--- counting_coins.py
+++ counting_coins.py
@@ -3,8 +3,12 @@ master = Tk()
 master.title('Counting coins')
 w = Canvas(master, width = 100, height=100)
 w.pack()
-two_coins = IntVar()
+
 
 compute_button = Button(master, text='Calculate', width=25, command=master.destroy)
 compute_button.pack()
 master.mainloop()
+
+if __name__ == "__main__":
+    pass
+
